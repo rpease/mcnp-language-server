bd60948f6f6cf18108323eb90aac52dc7c116275
Ryan Pease <therolando@aol.com>
2019-05-06 20:25:22 -0500

    Get a test script to finall start working

---
+++ server/test/utilities.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import * as utilities from '../src/utilities';
+
+describe('Utilities', () => 
+{
+	it('This is a test', () => 
+	{		
+		expect(1).to.equal(1);
+	});    
+});
