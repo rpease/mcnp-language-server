6aad1a8a53deddb2a60982248c10f980b4cb2909
Ryan Pease <therolando@aol.com>
2019-08-04 21:11:11 -0500

    Finish creating surface tests

--- server/test/surface.test.ts
+++ server/test/surface.test.ts
@@ -3,6 +3,7 @@ import { MCNPLine } from '../src/File/MCNPLines';
 import { Statement } from '../src/File/statement';
 import { Surface } from '../src/Cards/Surfaces/surface';
 import { MCNPException } from '../src/mcnp_exception';
+import { SurfaceModifier } from '../src/enumerations';
 
 function StringToStatement(text:string): Statement
 {
@@ -94,7 +95,7 @@ describe('Surface', () =>
 		let surface_codes = ['rpp', 'px', 'taco', 'doesntmatter'];
 		let parameters = '-4 5.4 3.7e2 1 10';
 
-		let num_formats = ['','.0','e0','E1',,'e+1'];
+		let num_formats = ['','.0','e0','E0',,'e+0'];
 
 		let parameter_split = parameters.split(' ');
 		for (let id = 1; id < 20; id++) 
@@ -104,11 +105,11 @@ describe('Surface', () =>
 				if(tr == 0)
 					continue;
 
-				for (const iterator of num_formats) 
+				for (const f of num_formats) 
 				{
 					for (const code of surface_codes) 
 					{
-						let surface_string = `${id} ${tr}${num_formats} ${code} ${parameters} $ But does it Djent?`;
+						let surface_string = `${id} ${tr}${f} ${code} ${parameters} $ But does it Djent?`;
 						let surf = StringToSurface(surface_string);
 		
 						expect(surf.ID).to.be.equal(id);
@@ -148,31 +149,101 @@ describe('Surface', () =>
 
 	it('ModifiedSurface', () => 
 	{	
-		let surface_codes = ['rpp', 'px', 'taco', 'doesntmatter'];
+		let surface_codes = ['rpp', 'px', 'c/x', 'doesntmatter'];
+		let parameters = '-4 5.4 3.7e2 1 10';
 		let modifiers = ['', '*', '+'];
 
+		let parameter_split = parameters.split(' ');
 		for (let id = 1; id < 20; id++) 
 		{
-			expect(true).to.be.false;
+			for (const mod of modifiers)
+			{				
+				for (const code of surface_codes) 
+				{
+					let surface_string = `${mod}${id} ${code} ${parameters} $ Thall`;
+					console.log(surface_string);
+					let surf = StringToSurface(surface_string);
+
+					let expected_mod = null;
+					if(mod == '*')
+						expected_mod = SurfaceModifier.Reflective;
+					else if (mod == '+')
+						expected_mod = SurfaceModifier.WhiteBoundary;
+						
+					expect(surf.ID).to.be.equal(id);
+					expect(surf.Modifier).to.be.equal(expected_mod);
+					expect(surf.Transform).to.be.null;
+					expect(surf.Parameters.length).to.equal(parameter_split.length);
+					for (let p = 0; p < surf.Parameters.length; p++)				
+						expect(surf.Parameters[p].Contents).to.be.equal(parameter_split[p]);						
+				}									
+			}					
 		}
 	});	
 
 	it('BadModifiers', () => 
 	{	
+		let surface_codes = ['rpp', 'px', 'c/x', 'doesntmatter'];
+		let parameters = '-4 5.4 3.7e2 1 10';
 		let bad_modifiers = ['\\', '-', '!', 'c', '$', '#', '@'];
-		expect(true).to.be.false;
+		
+		let parameter_split = parameters.split(' ');
+		for (let id = 1; id < 20; id++) 
+		{
+			for (const mod of bad_modifiers)
+			{				
+				for (const code of surface_codes) 
+				{
+					let surface_string = `${mod}${id} ${code} ${parameters} $ Thall`;					
+					console.log(surface_string);
+					let surf_statement = StringToStatement(surface_string);
+
+					expect(() => new Surface(surf_statement), "Should have thrown and error.").to.throw(MCNPException);
+				}									
+			}					
+		}
 	});	
 
 	it('ComplexSurface', () => 
 	{	
-		let surface_codes = ['rpp', 'px', 'taco', 'doesntmatter'];
+		let surface_codes = ['rpp', 'px', 'c/x', 'doesntmatter'];
+		let parameters = '-4 5.4 3.7e2 1 10';
 		let modifiers = ['', '*', '+'];
+		let num_formats = ['','.0','e0','E0',,'e+0'];
 
-		for (let id = 1; id < 20; id++) 
-		{
-			for (let tr = 1; tr < 10; tr++) 
-			{				
-				expect(true).to.be.false;
+		let parameter_split = parameters.split(' ');
+		
+		for (const mod of modifiers) 
+		{		
+			for (let id = 1; id < 20; id++) 
+			{
+				for (let tr = -10; tr < 10; tr++) 
+				{
+					if(tr == 0)
+						continue;
+
+					for (const f of num_formats) 
+					{
+						for (const code of surface_codes) 
+						{
+							let surface_string = `${mod}${id} ${tr}${f} ${code} ${parameters} $ Are you feeling it now?!`;
+							let surf = StringToSurface(surface_string);
+
+							let expected_mod = null;
+							if(mod == '*')
+								expected_mod = SurfaceModifier.Reflective;
+							else if (mod == '+')
+								expected_mod = SurfaceModifier.WhiteBoundary;
+			
+							expect(surf.ID).to.be.equal(id);
+							expect(surf.Modifier).to.be.equal(expected_mod);
+							expect(surf.Transform).to.be.equal(tr);
+							expect(surf.Parameters.length).to.equal(parameter_split.length);
+							for (let p = 0; p < surf.Parameters.length; p++)				
+								expect(surf.Parameters[p].Contents).to.be.equal(parameter_split[p]);						
+						}	
+					}				
+				}					
 			}
 		}
 	});
@@ -180,12 +251,63 @@ describe('Surface', () =>
 	it('BadIDNumbers_AlwaysWrong', () => 
 	{	
 		// surface ID must be: 1 <= n <= 99999
-		expect(true).to.be.false;
+		let surface_codes = ['rpp', 'px', 'taco', 'doesntmatter'];
+		let parameters = '-4 5.4 3.7e2 1 10';
+		let ids = [-1,0,1,99999,100000]
+
+		let parameter_split = parameters.split(' ');
+		for (const id of ids) 
+		{
+			for (const code of surface_codes) 
+			{
+				let surface_string = `${id} ${code} ${parameters} $ Magic spider on the wind`;
+				let surf_statement = StringToStatement(surface_string);
+
+				let surf: Surface;
+				// Invalid Number
+				if(id <= 0 || id >99999)
+					expect(() => new Surface(surf_statement), "Should have thrown and error.").to.throw(MCNPException);
+				// valid number
+				else
+					surf = new Surface(surf_statement);			
+			}			
+		}
 	});
 
 	it('BadIDNumbers_Transformation', () => 
 	{	
-		// surface ID must be: 1 <= n <= 999
-		expect(true).to.be.false;
+		let surface_codes = ['rpp', 'px', 'taco', 'doesntmatter'];
+		let parameters = '-4 5.4 3.7e2 1 10';
+
+		let num_formats = ['','.0','e0','E0',,'e+0'];
+
+		let ids = [-1,0,1,999,1000]
+
+		let parameter_split = parameters.split(' ');
+		for (const id of ids) 
+		{
+			for (let tr = -10; tr < 10; tr++) 
+			{
+				if(tr == 0)
+					continue;
+
+				for (const f of num_formats) 
+				{
+					for (const code of surface_codes) 
+					{
+						let surface_string = `${id} ${tr}${f} ${code} ${parameters} $ But does it Djent?`;
+						let surf_statement = StringToStatement(surface_string);
+
+						let surf: Surface;
+						// Invalid Number
+						if(id <= 0 || id >999)
+							expect(() => new Surface(surf_statement), "Should have thrown and error.").to.throw(MCNPException);
+						// valid number
+						else
+							surf = new Surface(surf_statement);						
+					}	
+				}				
+			}					
+		}
 	});
 });
