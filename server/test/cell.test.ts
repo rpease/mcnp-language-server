c897309adab915edd5050c501dd26a9211f9268a
Ryan Pease <therolando@aol.com>
2019-09-11 22:10:15 -0500

    Create more unit tests for scenarios I tried running in actual MCNP

--- server/test/cell.test.ts
+++ server/test/cell.test.ts
@@ -88,7 +88,38 @@ describe('Cell', () =>
 		expect(true).to.be.false;
 	});
 
-	it('Compliment', () => 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+	it('SurfaceID_Valid', () => 
+	{
+		let cell_ids = ['1','2','3','99999'];
+		let materials = ['1','2','3','4','999'];
+		let densities = ['-1.0','-2.0','-3','1.0','2.0','3.0'];
+		let surfaces = ['-666 4','3:4.2:8','(-10:8):(5.3 3.4 -3) -666','-10 8.2 (5 6 -2) -666'];
+		expect(true).to.be.false;		
+	});
+
+	it('SurfaceID_Error', () => 
+	{
+		let cell_ids = ['1','2','3','99999'];
+		let materials = ['1','2','3','4','999'];
+		let densities = ['-1.0','-2.0','-3','1.0','2.0','3.0'];
+		let bad = ['0','+1', '+2e0', '+2E+0'];	
+		expect(true).to.be.false;
+	});
+
+	it('SurfaceID_Warning', () => 
+	{
+		let cell_ids = ['1','2','3','99999'];
+		let materials = ['1','2','3','4','999'];
+		let densities = ['-1.0','-2.0','-3','1.0','2.0','3.0'];
+		let bad = ['1e0','1E0','1e+0','1.5e0'];	
+		expect(true).to.be.false;
+	});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+	it('Cell_Compliment_Valid', () => 
 	{
 		let cell_ids = ['1','2','3','99999'];
 		let materials = ['1','2','3','4','999'];
@@ -97,14 +128,140 @@ describe('Cell', () =>
 		expect(true).to.be.false;		
 	});
 
-	it('Compliment_NotValid', () => 
+	it('Cell_Compliment_Error', () => 
 	{
 		let cell_ids = ['1','2','3','99999'];
 		let materials = ['1','2','3','4','999'];
 		let densities = ['-1.0','-2.0','-3','1.0','2.0','3.0'];
-		let bad = ['#4.1','#-2','-#2','#2.0']; // todo is the last one invalid?		
+		let bad = ['#0','#-2','-#2','#-2.0','-#2.0',];		
 		expect(true).to.be.false;
 	});
+
+	it('Cell_Compliment_Warning', () => 
+	{
+		let cell_ids = ['1','2','3','99999'];
+		let materials = ['1','2','3','4','999'];
+		let densities = ['-1.0','-2.0','-3','1.0','2.0','3.0'];
+		let bad = ['#4.1','#2.0','#1e0','#1E0','#1e+0','#1.5e0'];		
+		expect(true).to.be.false;
+	});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+	it('Parentheses_Comment_Split', () => 
+	{
+		// These are valid examples
+		let examples = [];
+		examples.push(`666 0
+		(#1
+		#2
+		$ This is a comment
+		#100) imp:n=1 $ Graveyard`);
+
+		examples.push(`666 0
+		(#1
+		#2
+c This is a comment
+		#100) imp:n=1 $ Graveyard`);
+
+		expect(true).to.be.false;
+	});	
+
+	it('Incomplete_Parentheses', () => 
+	{
+		let examples = [];
+
+		// example with trailing parameters
+		examples.push('1 1 -10.0  (-1 100( 8 9:10) imp:n=0');
+
+		// no trailing parameters
+		examples.push('1 1 -10.0  (-1 100( 8 9:10) imp:n=0');
+
+		// In-line comment break
+		examples.push(`666 0
+		(#1
+		#2
+		$ This is a comment
+		#100 imp:n=1 $ Graveyard`);
+
+		// full-line comment break
+		examples.push(`666 0
+		(#1
+		#2
+c This is a comment
+		#100 imp:n=1 $ Graveyard`);
+
+		expect(true).to.be.false;
+	});	
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+	it('Colon_Comment_Split', () => 
+	{
+		// These are valid examples
+		let examples = [];
+		examples.push(`666 0
+		#1
+		#2 :
+		$ This is a comment
+		#100 imp:n=1 $ Graveyard`);
+
+		examples.push(`666 0
+		(#1
+		#2 :
+c This is a comment
+		#100) imp:n=1 $ Graveyard`);
+
+		expect(true).to.be.false;
+	});	
+
+	it('Valid_Colon', () => 
+	{
+		let examples = [];
+
+		// No numbers near by
+		examples.push('1 1 -10.0  (3 -4):(1 4) imp:n=0');
+
+		// behind number
+		examples.push('1 1 -10.0  ((3 -4):1) imp:n=0');
+
+		expect(true).to.be.false;
+	});	
+
+	it('Incomplete_Colon', () => 
+	{
+		let examples = [];
+
+		// No numbers near by
+		examples.push('1 1 -10.0  (-1 100( 8 9:10):) imp:n=0');
+
+		// behind number
+		examples.push('1 1 -10.0  (-1 100( 8 9:10:)) imp:n=0');
+
+		// before any surface/cell
+		examples.push('1 1 -10.0  : -1 100 8 9 10 imp:n=0');
+
+		// after all surface/cell
+		examples.push('1 1 -10.0  : -1 100 8 9 10 imp:n=0');
+
+		// In-line comment break
+		examples.push(`666 0
+		(#1
+		#2
+		$ This is a comment
+		#100 imp:n=1 $ Graveyard`);
+
+		// full-line comment break
+		examples.push(`666 0
+		(#1
+		#2
+c This is a comment
+		#100 imp:n=1 $ Graveyard`);
+
+		expect(true).to.be.false;
+	});	
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 	it('SimpleCell_ZeroDensityWarning', () => 
 	{
